0a4f790a855e8644ebe271ebf1813b9e5e8073c2
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2024-11-20 20:42:19 -0300

    Modificadores de acesso

--- src/aula16.ts
+++ src/aula16.ts
@@ -5,16 +5,18 @@
 // Polimorfismo
 
 class Computador {
-  nome: string;
-  ram: number;
-  cpu: number;
-  ligado: boolean;
+  private id: number = 0;
+  public nome: string;
+  private ram: number;
+  private cpu: number;
+  private ligado: boolean;
 
   constructor(nome: string, ram: number, cpu: number) {
     this.nome = nome;
     this.ram = ram;
     this.cpu = cpu;
     this.ligado = false;
+    this.id = 0;
   }
 
   info(): void {
@@ -32,6 +34,16 @@ class Computador {
   desligar() {
     this.ligado = false;
   }
+
+  upRam(qtde: number): void {
+    if (qtde >= 0 && qtde <= 256) {
+      this.ram = qtde;
+    } else {
+      console.log(
+        `Quantidade ${qtde} para o computador ${this.nome} não é permitida!`
+      );
+    }
+  }
 }
 
 //Instanciar
@@ -41,6 +53,10 @@ const comp3 = new Computador("Gamer", 128, 10);
 
 comp1.ligar();
 comp3.ligar();
+
+comp1.upRam(128);
+
+comp1.nome = "Super Rapidão";
 
 comp1.info();
 comp2.info();
