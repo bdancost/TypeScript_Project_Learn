d8612dba67ff6110c82f4f0746fcc2044ee6b939
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2024-11-25 22:45:39 -0300

    Removendo arquivo JS do rastreamento

--- build/aula07.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-let coisas = ["João", 30, true];
-//coisas.push("Daniel", 40, false);
-console.log(coisas);
-console.log(coisas[0]);
--- build/aula11.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let nvalor;
-let svalor;
-let uvalor;
-uvalor = 10;
-svalor = "20";
-nvalor = Number.parseInt(svalor);
